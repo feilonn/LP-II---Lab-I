a4eb823055125516162f69c3c950a227b8d13611
Raul Vinicius dos Santos Correia <60004704+feilonn@users.noreply.github.com>
2020-03-25 23:15:43 -0300

    Update questaoOnze.java

--- questaoOnze.java
+++ questaoOnze.java
@@ -1,6 +1,4 @@
-package treinoJava;
 
-public class questaoOnze{
     public static void main(String[] args){
         
         for(int i = 0; i < 5; i++){
@@ -24,31 +22,3 @@ public class questaoOnze{
         }
         
     }     
-}
-
-
-/*
-public static void main(String[] args){
-        
-        for(int i = 0; i < 5; i++){
-            if(i <= 4){
-                for(int k = 0; k < 4 - i; k++){
-                    System.out.print(" ");
-                }
-            }
-            for(int j = 0; j < ((10 - (9 - i) ) + i ); j++){
-                System.out.print("*");
-            }System.out.println();
-        }
-        
-        for(int i = 0; i < 5; i++){
-            for(int k = 0; k < (i+1); k++){
-                System.out.print(" ");
-            }
-            for(int j = 0; j < (7 - (i*2) ); j++){
-                System.out.print("*");
-            }System.out.println();
-        }
-        
-    }
-*/
